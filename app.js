165d80a82af087761626f86c52d88a637c4e429b
Rebecca-10 <beckie04@gmail.com>
2022-08-18 16:13:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -32,19 +32,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.get('/')
 
-// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("not-found");
+  err.status = 404;
+  err.message = "404 error ";
+  next(err);
+});
+
+//error outside 
+app.use((err, req, res) => {
+  if (err) {
+    if (err.status === 404) {
+      res.status(404)
+      render(err.message, { err });
+    } else {
+      err.message = "Sorry, Error with the server";
+      res.status(500).render('error', { err });
+    }
+  }
+});
+//404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the page with error message
   res.status(err.status || 500);
   res.render('error');
 });
