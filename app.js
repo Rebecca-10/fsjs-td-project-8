5570b0089e86ca09f3faf2d151fcfecdb7028052
Rebecca-10 <beckie04@gmail.com>
2022-08-19 07:31:43 -0700

    not sure anymore

--- app.js
+++ app.js
@@ -34,26 +34,36 @@ app.use('/', indexRouter);
 
 //error inside for the routes 
 app.use((req, res, next) => {
-  var err = new Error('Sorry, Error')
-  console.log('404 error handler called');
+  const err = new Error("not-found");
   err.status = 404;
-  res.render('page-not-found', { err })
-  });
-
-//error outside 
-
-  app.use((err, req, res, next) => {
-
-    console.log('500 error being handled');
-    err.status = 500;
-    err.message = `Sorry, Error from server.`
-    console.log(err.status);
-    console.log(err.message);
-    res.status(err.status || 500);
-    res.render('error', { err });
-  
+  err.message = "404 error ";
+  next(err);
 });
 
+//error outside 
+app.use((err, req, res) => {
+  if (err) {
+    if (err.status === 404) {
+      res.status(404)
+      render(err.message, { err });
+    } else {
+      err.message = "Sorry, Error with the server";
+      res.status(500).render('error', { err });
+    }
+  }
+});
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
