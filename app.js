6ef369c7a19b61b38677c7059d6d25cb7abcb6a0
Rebecca-10 <beckie04@gmail.com>
2022-08-18 17:56:00 -0700

    new
    still having trouble with catching errors

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use('/', indexRouter);
 
 app.get('/')
 
+//error inside for the routes 
 app.use((req, res, next) => {
   const err = new Error("not-found");
   err.status = 404;
@@ -53,20 +54,16 @@ app.use((err, req, res) => {
     }
   }
 });
-//404
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
 
-  // render the page with error message
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 module.exports = app;
